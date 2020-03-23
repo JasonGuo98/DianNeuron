795bb6f5b58f08d07248325b414691cfab7831a0
jasonguo98 <jasonguo98926@gmail.com>
2020-03-23 21:01:03 +0800

    [add] add visualize tool for netwark

---
+++ utils/visualizeTool.py
@@ -0,0 +1,49 @@
+import networkx as nx
+import numpy as np
+import matplotlib.pyplot as plt
+import pylab
+
+__all__ = ["get_graph_noed_and_edge","plot_network"]
+
+color_dic = {"Input":1,"Dense":0.75,"ADD":0.5,"Dropout":0.25}
+def get_graph_noed_and_edge(input_layer,output_layer):
+    edges = []
+    edge_labels = {}
+    all_layer = [input_layer]
+    for layer in all_layer:
+        if hasattr(layer,"activation"):
+            layer_name = layer.name + "=>"+layer.activation.name
+        else:
+            layer_name = layer.name
+        for next_layer in layer.next_layer_list:
+            if hasattr(next_layer,"activation"):
+                next_layer_name = next_layer.name + "=>"+next_layer.activation.name
+            else:
+                next_layer_name = next_layer.name
+            all_layer.append(next_layer)
+            edges.append([layer_name,next_layer_name])
+            edge_labels[(layer_name,next_layer_name)] = "BatchSize"+"×"+str(layer.out_dim)
+    return edges,edge_labels
+
+def plot_network(input_layer,output_layer):
+    G = nx.DiGraph()
+    edges,edge_labels = get_graph_noed_and_edge(input_layer,output_layer)
+    G.add_edges_from(edges, weight=1)
+    
+    node_color = []
+    for node in G.nodes:
+        print(node)
+        got_color = False
+        for k,v in color_dic.items():
+            if node.startswith(k):
+                node_color.append(v)
+                got_color = True
+                break
+        if not got_color:
+            node_color.append(0)
+    
+    pos=nx.spectral_layout(G)
+    pylab.figure(figsize=(20,10))
+    nx.draw_networkx_edge_labels(G,pos,edge_labels=edge_labels)
+    nx.draw(G,pos, with_labels=True,cmap = "rainbow",node_color = node_color, arrowsize = 20,node_size=1500,edge_cmap=plt.cm.Reds)
+    pylab.show()
